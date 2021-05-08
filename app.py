1128cad9c69c7ff09b787ee44f43f0f67003d2fe
Prajith Kesava Prasad <pkesavap@redhat.com>
2021-05-08 13:52:26 +0530

    Implemented UpdateAPI for email,host
    Signed-off-by: Prajith Kesava Prasad <pkesavap@redhat.com>

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,session,logging,url_for,redirect,flash,jsonify
+from flask import Flask,render_template,request,session,logging,url_for,redirect,flash,jsonify,abort
 from flask_sqlalchemy import SQLAlchemy
 #from sqlalchemy import create_engine
 #from sqlalchemy.orm import scoped_session, sessionmaker
@@ -19,17 +19,13 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
 class Network(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(50), nullable=False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    email = db.Column(db.String(50),  nullable=False)
     hosturl = db.Column(db.String(300), nullable=False)
-
-
-
-
 
 @app.route("/registerNetwork", methods=["POST"])
 def registerNetwork():
-  
+    db.init_app(app)
     db.create_all()
     #flash("File Uploaded", "success")
     print("praji")
@@ -41,12 +37,34 @@ def registerNetwork():
         resp.status_code = 200
         email = new_map['email_field']
         hosturl = new_map["host_url_field"]
+        email_db = Network.query.filter_by(email=email).first()
+        if email_db is not None:
+            abort(404, f'email {email} already registered, please update the db')
+
         new_map = Network(email=email, hosturl=hosturl)
         db.session.add(new_map)
         db.session.commit()
         return resp
  
+@app.route("/updateNetwork", methods=["POST"])
+def updateNetwork():
+    resp = jsonify(success=True)
+    if request.method == "POST":
+        new_map= request.json
+        email = new_map['email_field']
+        hosturl = new_map['host_url_field']
+        email_db = Network.query.filter_by(email=email).first()
+        if email_db is None:
+            abort(404, f'email {email} does not exist')
+        else:
+            num_rows_updated = Network.query.filter_by(email=email).update(dict(hosturl=hosturl))
+            db.session.commit()
+            resp.status_code = 200
+            return resp
+    resp.status_code=404
+    return resp
 
+        
     
 
 @app.route("/")
